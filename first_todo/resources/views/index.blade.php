3c2f1bed74d9e27f7ef8123a333bbb2d0c5222b1
KikuchiHaruto <o5o206felt@icloud.com>
2021-11-25 19:04:31 +0900

    indexの編集。

--- first_todo/resources/views/index.blade.php
+++ first_todo/resources/views/index.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.default')
+<style>
+  tr{
+    display:flex;
+    justify-content:center;
+  }
+  th{
+    display:block;
+  }
+</style>
 
 @section('title','Index')
 
+@section('content')
+<table>
+  @foreach($items as $item)
+  <tr>
+    <th>作成日</th>
+    <td>{{$item->create_at}}</td>
+  </tr>
+  <tr>
+    <th>タスク名</th>
+    <td>{{$item->content}}</td>
+  </tr>
+  @endforeach
+</table>
+@endsection
 
